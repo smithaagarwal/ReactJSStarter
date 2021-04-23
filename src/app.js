009f2762d47bfb034407c92f016a872e15513cb6
Smitha <smitha.kotian@gmail.com>
2021-04-23 11:04:49 +0100

    Simple JSX example with buttons

--- src/app.js
+++ src/app.js
@@ -4,8 +4,46 @@ import ReactDom from 'react-dom';
 
 export default class App extends Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            numbers : []
+        }
+    }
+
+    load() {
+        this.setState(
+            {numbers: [1,2,3,4,5,6]}
+        );
+    }
+
+    increment() {
+        this.setState(
+            {numbers: this.state.numbers.map(value => value+1)}
+        );
+    }
    
-    render() {
-        return 'Hello';
+     // render() {
+    //     const lines = this.state.numbers.map((number,index) =>
+    //     createElement('li', {key:index.toString()}, number));
+    //     return (createElement('div' , null ,
+    //     createElement('h2',null,'Numbers'),
+    //     createElement('ul', null, lines),
+    //     createElement('button',{onClick:()=>this.load()},'load'),
+    //     createElement('span',null,'       '),
+    //     createElement('button',{onClick:() => this.increment()},'increment')
+    //     ));
+    // }
+
+    render () {
+        const lines = this.state.numbers.map((number, index) =>
+        <li key={index.toString() }>{number}</li>);
+
+        return <div>
+            <h2>Numbers</h2>
+            <ul>{lines}</ul>
+            <button onClick = {() => this.load()}>load</button>
+            <button onClick = {() => this.increment() }>increment</button>
+        </div>
     }
 }
