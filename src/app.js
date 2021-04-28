f14258d7e4e406c7546b4d68dab9ab4cd94761e6
Smitha <smitha.kotian@gmail.com>
2021-04-28 11:39:55 +0100

    Sample for property type validation using prop-types

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ export default class App extends Component {
    
     render() {
         //Outer curly us for specifying expression and inner curly is for specifying map
+        //to ensure property validation age property with string 2 is sent 
         return <span>
             <p style={{backgroundColor:'red', color:'white'}}>React </p>
             <span className="highlighted">Hello </span>
-            <Greet name='Bob'></Greet>
-            <Greet name='Bobo'></Greet>
+            <Greet name='Bob' age={20}></Greet>
+            <Greet name='Bobo'age={24}></Greet>
+            <Greet name = 'Sara' age="2"></Greet> 
             <Greet />
         </span>;
     }
--- src/greet.js
+++ src/greet.js
@@ -1,4 +1,5 @@
 import React , {Component} from 'react';
+import PropTypes from 'prop-types';
 
 export default class Greet extends Component {
     constructor(props){
@@ -8,12 +9,19 @@ export default class Greet extends Component {
         return( 
         <div>    
         <div>
-            My name is : {this.props.name}
+            My name is : {this.props.name} and I am {this.props.age} years old
         </div>
         <div>My name length is:{this.props.name.length}</div> 
         </div>)
     }
 }
 Greet.defaultProps = {
-    name : 'Jimmy'
+    name : 'no name entered' ,
+    age : 0 
 };
+//the below validation happens for properties passed and not for the default properties. 
+//so if there is a type mistake in default it will not validate and throw an error in the console
+Greet.propTypes = {
+    name : PropTypes.string,
+    age : PropTypes.number
+}
