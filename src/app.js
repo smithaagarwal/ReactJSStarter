8c73b0e4c42479d9bbe04ef1ac7df6bb37434ccd
Smitha <smitha.kotian@gmail.com>
2021-04-28 10:43:46 +0100

    Added comments

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ export default class App extends Component {
     
    
     render() {
+        //Outer curly us for specifying expression and inner curly is for specifying map
         return <span>
             <p style={{backgroundColor:'red', color:'white'}}>React </p>
             <span className="highlighted">Hello </span>
