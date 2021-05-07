cb3ae3ee1cc8b93ceb5b91f6f69a73f620351e97
Smitha <smitha.kotian@gmail.com>
2021-05-07 11:36:06 +0100

    Basic button events

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
 import React, {Component, ComponentElement} from 'react';
-import ReactDom from 'react-dom';
-
 
 export default class App extends Component {
-    
+    constructor(props) {
+        super (props);
+
+        this.state= {
+            count: 0
+        }
+    }
+
+    incrementCounter() {
+        this.setState({
+            count:this.state.count+1
+        });
+    }
+
+    decrementCounter() {
+        this.setState({
+            count:this.state.count-1
+        });
+    }
    
     render() {
-        return 'Hello';
+        return <div>
+            <p> Count : {this.state.count}</p>
+            <button onClick = {()=> this.incrementCounter()}>Increment</button>
+            <span>  </span>
+            <button onClick = {()=> this.decrementCounter()}>Decrement</button>
+        </div>;
     }
 }
